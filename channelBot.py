d874a2728ed7583ad9149f51385cb930aacff426
Erin <erin001tw@gmail.com>
2023-09-19 16:36:32 +0800

    get API from tx

--- channelBot.py
+++ channelBot.py
@@ -1,34 +1,64 @@
 import requests
 import json
 from config import TOKEN
-#  from utils.utils_api import tx
+# from utils.utils_api import tx
+
+TOKEN = "6538289312:AAHLHWTIlwqJ8CpeyX8XFW4rIZge9tgbem4"
+chat_id = "-1001841995638"
+
+#API URL to get transaction information
+api_url = "https://"
+
+response = requests.get(api_url)
+
+if response.status_code == 200:
+    transaction_data = response.json()
+    
+    tx_id = transaction_data["tx_id"]
+    block_id = transaction_data["block_id"]
+    sender_address = transaction_data["sender_address"]
+    receiver_address = transaction_data["receiver_address"]
+    transaction_type = transaction_data["type"]
+    amount = transaction_data["amount"]
+    confirm_time = transaction_data["confirm_time"]
+    
+    notification = f"""
+    **Large Transaction Alert**
+    > Notification: A large transaction has been detected! 🚨
+    > 
+    > **Sender's Address**: *{sender_address}*
+    > 
+    > **Recipient's Address**: *{recipient_address}*
+    > 
+    > **Transaction Amount**: *{amount}* TON
+    > 
+    > **Transaction Time**: *{confirm_time}*
+    > 
+    > Please take note of this transaction for security and transparency.
+    """
+    
+    message = {
+            'chat_id': chat_id,
+            'text': notification,
+            'parse_mode': 'MarkdownV2'
+        }
+    
+    # Send the message to the Telegram channel
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    response = requests.post(url, json=message)
+        
+    if response.status_code == 200:
+            print('Message successfully sent to the Telegram channel!')
+    else:
+            print('Message sending failed. Status code:', response.status_code)
+            print('Error message:', response.text)
+else:
+    print('API request failed. Status code:', response.status_code)
 
 
-#Database realtime tx alert
 
-sender_address = "Sender's address here"
-recipient_address = "Recipient's address here"
-transaction_amount = "Transaction amount here"
-transaction_time = "Transaction time here"
-
-notification = f"""
-**Large Transaction Alert**
-> Notification: A large transaction has been detected! 🚨
-> 
-> **Sender's Address**: *{sender_address}*
-> 
-> **Recipient's Address**: *{recipient_address}*
-> 
-> **Transaction Amount**: *{transaction_amount}* TON
-> 
-> **Transaction Time**: *{transaction_time}*
-> 
-> Please take note of this transaction for security and transparency.
-"""
-
-
-def send_tx_announcement(tx:[tx]):
-    pass
+# def send_tx_announcement(tx:[tx]):
+#     pass
     # address_queue = []
     # for amount in tx:
     #     if amount["amount"] > 100:
@@ -39,11 +69,7 @@ def send_tx_announcement(tx:[tx]):
 
 
 
-requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id=-1001841995638&text={notification}')
-
-
-
-
+# requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id=-1001841995638&text={notification}')
 
 
 
@@ -55,4 +81,3 @@ requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id=-1001841
 #     return blockheight
 
 # print(get_block_height())
-
