21320fdeed951c7f28135fd41dd1ef256b43e001
Erin <erin001tw@gmail.com>
2023-09-20 23:12:50 +0800

    add API URL

--- channelBot.py
+++ channelBot.py
@@ -5,14 +5,19 @@ from config import TOKEN
 
 TOKEN = "6538289312:AAHLHWTIlwqJ8CpeyX8XFW4rIZge9tgbem4"
 chat_id = "-1001841995638"
+data = {
+    "amount": 100
+}
 
-#API URL to get transaction information
-api_url = "https://"
+# Mock tx info
+# api_url = "http://si8a1.asuscomm.com:8000/mock/"
 
-response = requests.get(api_url)
+# Real-Time tx info
+api_url = "http://si8a1.asuscomm.com:8000/filter_amount/"
+res = requests.post(api_url, json=data)
 
-if response.status_code == 200:
-    transaction_data = response.json()
+if res.status_code == 200:
+    transaction_data = res.json()
     
     tx_id = transaction_data["tx_id"]
     block_id = transaction_data["block_id"]
@@ -53,7 +58,7 @@ if response.status_code == 200:
             print('Message sending failed. Status code:', response.status_code)
             print('Error message:', response.text)
 else:
-    print('API request failed. Status code:', response.status_code)
+    print('API request failed. Status code:', res.status_code)
 
 
 
