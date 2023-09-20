786ff616c01e3ea5d8c2cd12d2e15d8da783a034
Erin <erin001tw@gmail.com>
2023-09-21 02:31:05 +0800

    import datetime

--- channelBot.py
+++ channelBot.py
@@ -1,64 +1,65 @@
 import requests
 import json
 from config import TOKEN
+from datetime import datetime
+import time
 # from utils.utils_api import tx
 
 TOKEN = "6538289312:AAHLHWTIlwqJ8CpeyX8XFW4rIZge9tgbem4"
 chat_id = "-1001841995638"
-data = {
-    "amount": 100
-}
 
 # Mock tx info
 # api_url = "http://si8a1.asuscomm.com:8000/mock/"
 
 # Real-Time tx info
-api_url = "http://si8a1.asuscomm.com:8000/filter_amount/"
-res = requests.post(api_url, json=data)
+# while True:
+api_url = "http://si8a1.asuscomm.com:8000/filteredNodes/?amount=100"
+res = requests.get(api_url)
 
 if res.status_code == 200:
     transaction_data = res.json()
-    
-    tx_id = transaction_data["tx_id"]
-    block_id = transaction_data["block_id"]
-    sender_address = transaction_data["sender_address"]
-    receiver_address = transaction_data["receiver_address"]
-    transaction_type = transaction_data["type"]
-    amount = transaction_data["amount"]
-    confirm_time = transaction_data["confirm_time"]
+    notifications = []
+
+    for transaction in transaction_data:
+        tx_id = transaction["Transaction_id"]
+        block_id = transaction["Block_id"]
+        sender_address = transaction["Sender_address"]
+        receiver_address = transaction["Receiver_address"]
+        transaction_type = transaction["Type"]
+        amount = transaction["Amount"]
+        confirm_time = datetime.utcfromtimestamp(transaction["Confirm_time"]).strftime('%Y-%m-%d %H:%M:%S')
     
     notification = f"""
-    **Large Transaction Alert**
-    > Notification: A large transaction has been detected! 🚨
-    > 
-    > **Sender's Address**: *{sender_address}*
-    > 
-    > **Recipient's Address**: *{recipient_address}*
-    > 
-    > **Transaction Amount**: *{amount}* TON
-    > 
-    > **Transaction Time**: *{confirm_time}*
-    > 
-    > Please take note of this transaction for security and transparency.
+🚨 *LARGE TRANSACTION ALERT* 🚨
+
+*Sender's Address*: {sender_address}
+*Recipient's Address*: {receiver_address}
+*Transaction Amount*: {amount} TON
+*Transaction Time*: {confirm_time} UTC
+
+⚠️ Please take note of this transaction for security and transparency ⚠️
     """
-    
-    message = {
-            'chat_id': chat_id,
-            'text': notification,
-            'parse_mode': 'MarkdownV2'
-        }
-    
-    # Send the message to the Telegram channel
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    response = requests.post(url, json=message)
+    notifications.append(notification)
+
+    for notification in notifications:
+        message = {
+                'chat_id': chat_id,
+                'text': notification,
+                'parse_mode': 'Markdown'
+            }
         
-    if response.status_code == 200:
-            print('Message successfully sent to the Telegram channel!')
-    else:
-            print('Message sending failed. Status code:', response.status_code)
-            print('Error message:', response.text)
+    # Send the message to the Telegram channel
+        url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+        response = requests.post(url, json=message)
+            
+        if response.status_code == 200:
+                print('Message successfully sent to the Telegram channel!')
+        else:
+                print('Message sending failed. Status code:', response.status_code)
+                print('Error message:', response.text)
 else:
     print('API request failed. Status code:', res.status_code)
+    # time.sleep(60)
 
 
 
